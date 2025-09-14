90606f2b99ab555fa68e5f9637968ac7f7f0e22a
RahulVts007 <rahulvts89@gmail.com>
2025-09-14 14:39:07 +0530

    Fix Password Error

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const {Pool} = pkg;
 dotenv.config(); /// load .env variable into process.env object
 
 const pool = new Pool ({
-    user: process.env.DB,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
